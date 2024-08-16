67cfe6238e71ac62c9fe8a11c1f8054b4e9777f1
K <wa2000.pen.megane@gmail.com>
2024-08-16 15:52:25 +0900

    Fix Cluster scripts

--- Cluster/Kmeans_pca.py
+++ Cluster/Kmeans_pca.py
@@ -12,19 +12,19 @@ X = iris.data
 y = iris.target
 print(f"X.shape{X.shape}")
 
-# K-means
-kmeans = KMeans(n_clusters=3, random_state=0)
-kmeans.fit(X)
-
 # PCA
 pca = PCA(n_components=2) # transform into 2d
 X_pca = pca.fit_transform(X)
 print(f"X_pca.shape({X_pca.shape})")
+
+# K-means
+kmeans = KMeans(n_clusters=3, random_state=0)
+kmeans.fit(X_pca)
 
 # Plot scatter graph
 scatter = plt.scatter(X_pca[:, 0], X_pca[:, 1], c=y, cmap='viridis')
 plt.colorbar(scatter, label='Cluster Level')
 plt.xlabel('PCA Component 1')
 plt.xlabel('PCA Component 2')
-plt.savefig('fig_Kmeans2PCA.png')
+plt.savefig('fig_KmeansPCA.png')
 plt.show()
